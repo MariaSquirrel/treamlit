18caa61b51ef16db12a70931ec4d7b910d84022e
MariaSquirrel <148685327+MariaSquirrel@users.noreply.github.com>
2024-09-04 14:49:29 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+
+# def print_hi(name):
+#     print(f'Hi, {name}')
+#
+# if __name__ == '__main__':
+#     print_hi('PyCharm ')
+
+
+# import streamlit as st
+# # st.write('Hello world!')
+#
+# st.header('st.button')
+#
+# if st.button('Say hello'):
+#     st.write('Why hello there')
+# else:
+#     st.write('Goodbye')
+
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
